5f3a1b173ee87c8cb7b1811cb91f4fa15f140f83
nate000088@gmail.com <nate000088@gmail.com>
2017-06-09 20:05:01 -0700

    ninjas and survey assignments

--- form/server.py
+++ form/server.py
@@ -3,8 +3,31 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
-	print 'home'
 	return render_template("index.html")
+
+@app.route('/process', methods=['POST'])
+def proc():
+	name = request.form['name']
+	print name
+	return render_template('process.html',name=request.form['name'],loc=request.form['loc'],lang=request.form['lang'],com=request.form['comment'])
+
+@app.route('/ninja')
+def ninjas():
+	return render_template("ninja.html",link="/static/Ninjas/tmnt.png")
+
+@app.route('/ninja/<color>')
+def ninja(color):
+	ret = 'notapril.jpg'
+	n = {'blue':'leonardo.jpg','orange':'michelangelo.jpg','red':'raphael.jpg','purple':'donatello.jpg'}
+	if color in n:
+		ret = n[color]
+	print"{{url_for('static',filename='Ninjas/" + ret + "')}}"
+	return render_template('ninja.html',link="/static/Ninjas/"+ret)
+		
+
+@app.route('/dojos/new')
+def dn():
+	return render_template("dojos.html")
 
 @app.route('/users',methods=['POST'])
 def create_user():
